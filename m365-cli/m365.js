77e0fac04f4e1cc7cb432465c39bdafe7ff29a0c
Roman Hatz <104928462+roman-hatz-sluz@users.noreply.github.com>
2023-10-22 09:11:04 +0200

    Create m365.js

---
+++ m365-cli/m365.js
@@ -0,0 +1,26 @@
+import { exec } from "child_process";
+
+// Define the command you want to execute
+const commandBase = "node_modules/.bin/m365";
+
+const commands = {
+  status: "",
+  login: "",
+  sendMsg: `${commandBase} teams chat message send --userEmails lfo@gmx.ch --message "Welcome to Teams <strong>bold</strong> ${Date.now()}"`,
+};
+
+// Execute the command
+exec(commands.sendMsg, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error executing command: ${error}`);
+    return;
+  }
+
+  console.log("Command output:");
+  console.log(stdout);
+
+  if (stderr) {
+    console.error("Command error:");
+    console.error(stderr);
+  }
+});
